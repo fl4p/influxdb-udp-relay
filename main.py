fdab2bf336a8bb5bb233a5104cac12cace53c9ef
Fabian Schlieper <fabian@schlieper.email>
2023-08-20 20:44:09 +0200

    add additional_servers

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import socket
 import queue
+import socket
 import time
 from threading import Thread
+from typing import List
 
 import influxdb
 
@@ -13,13 +14,56 @@ UDP_IP = "0.0.0.0"
 
 logger = get_logger()
 
-Q = queue.Queue(200_000)
+
+class InfluxDBWriter():
+
+    def _write_thread(self):
+        while True:
+            batch = []
+            while not self.Q.empty() and len(batch) < 20_000:
+                batch.append(self.Q.get())
+
+            if len(batch) > 0:
+                logger.info('Writing %d (max msg len=%d)', len(batch), max_msg_len)
+                try:
+                    self.client.write(batch, protocol='line', params=dict(db='open_pe', precision='ms'))
+                except Exception as e:
+                    logger.error('Error writing batch: %s', e)
+
+            time.sleep(.2)
+
+    def __init__(self, influxdb_conf):
+        host = influxdb_conf.get('host', "homeassistant.local")
+        user = influxdb_conf.get('username')
+
+        logger.info('Connecting InfluxDB %s@%s', user, host)
+
+        self.client = influxdb.InfluxDBClient(
+            host=host,
+            port=int(influxdb_conf.get('port', 8086)),
+            # username="home_assistant", password="h0me",
+            # username="hass", password="caravana",
+            username=user,
+            password=influxdb_conf.get('password'),
+            database=influxdb_conf.get('database'),
+            ssl=influxdb_conf.get('ssl', False),
+            verify_ssl=False, #influxdb_conf.get('ssl', False),
+        )
+
+        logger.info('Measurements: %s', ','.join(map(lambda m: m.get('name', m), self.client.get_list_measurements())))
+
+        self.Q = queue.Queue(200_000)
+
+        self.w_thread = Thread(target=self._write_thread, daemon=True)
+        self.w_thread.start()
+
+
 max_msg_len = 0
 
 opt = read_user_options()
 
 
-def receive_loop():
+def receive_loop(writers: List[InfluxDBWriter]):
     global max_msg_len
 
     udp_port = int(opt.get('udp_port', 8086))
@@ -36,7 +80,8 @@ def receive_loop():
         msg = data.decode("utf-8").rstrip('\n')
         lines = msg.split('\n')
         for l in lines:
-            Q.put(l, block=False)
+            for w in writers:
+                w.Q.put(l, block=False)
 
         # logger.info("msg (%dB, %dL, Q=%d) from %s: %s", len(data), len(lines), Q.qsize(), addr[0], msg[:60])
 
@@ -51,40 +96,17 @@ def main():
     # write = client.write_api(write_options=SYNCHRONOUS)
 
     config = ha.read_hass_configuration_yaml()
-    influxdb_conf = config.get('influxdb')
-    host = influxdb_conf.get('host', "homeassistant.local")
-    user = influxdb_conf.get('username')
+    influxdb_conf = config.get('influxdb', None)
 
-    logger.info('Connecting InfluxDB %s@%s', user, host)
+    writers = []
 
-    client = influxdb.InfluxDBClient(
-        host=host,
-        port=int(influxdb_conf.get('port', 8086)),
-        # username="home_assistant", password="h0me",
-        # username="hass", password="caravana",
-        username=user,
-        password=influxdb_conf.get('password'),
-        database=influxdb_conf.get('database'),
-    )
+    if influxdb_conf:
+        writers.append(InfluxDBWriter(influxdb_conf))
 
-    logger.info('Measurements: %s', ','.join(map(lambda m:m.get('name', m),client.get_list_measurements())))
+    for influxdb_conf in opt.get('additional_servers', []):
+        writers.append(InfluxDBWriter(influxdb_conf))
 
-    rx_thread = Thread(target=receive_loop, daemon=True)
-    rx_thread.start()
-
-    while True:
-        batch = []
-        while not Q.empty() and len(batch) < 20_000:
-            batch.append(Q.get())
-
-        if len(batch) > 0:
-            logger.info('Writing %d (max msg len=%d)', len(batch), max_msg_len)
-            try:
-                client.write(batch, protocol='line', params=dict(db='open_pe', precision='ms'))
-            except Exception as e:
-                logger.error('Error writing batch: %s', e)
-
-        time.sleep(.2)
+    receive_loop(writers)
 
 
 main()
